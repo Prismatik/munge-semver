61a522e609ff6652964b9b04b033de291e94cc9e
David Banham <david@banham.id.au>
2014-10-08 11:30:59 +1100

    Remove special characters

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var semver = require('semver');
 
 module.exports = function(version) {
-▸ if (semver.valid(version)) return version;
-▸ if (semver.valid('0.'+version)) return '0.'+version;
-▸ if (semver.valid('0.0.'+version)) return '0.0.'+version;
-▸ return new Error ("Cannot munge version: "+version);
+  if (semver.valid(version)) return version;
+  if (semver.valid('0.'+version)) return '0.'+version;
+  if (semver.valid('0.0.'+version)) return '0.0.'+version;
+  return new Error ("Cannot munge version: "+version);
 };
