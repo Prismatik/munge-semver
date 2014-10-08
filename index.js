74312d17ffed2852eaf62928e9ccd7d0fff02861
David Banham <david@banham.id.au>
2014-10-08 11:09:50 +1100

    First cut

---
+++ index.js
@@ -0,0 +1,8 @@
+var semver = require('semver');
+
+module.exports = function(version) {
+▸ if (semver.valid(version)) return version;
+▸ if (semver.valid('0.'+version)) return '0.'+version;
+▸ if (semver.valid('0.0.'+version)) return '0.0.'+version;
+▸ return new Error ("Cannot munge version: "+version);
+};
